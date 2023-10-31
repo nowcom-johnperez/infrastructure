e162ad55eee44593519861e3f012b9ccab619cdc
Francis Al Victoriano <fvictoriano@now-fvict-mbp.hankeyinvestments.com>
2023-10-31 16:53:34 -0700

    create nowcom plugins

---
+++ pkg/nowcom/pages/createnetwork.vue
@@ -0,0 +1,13 @@
+<script>
+export default { name: 'CreateNetwork' };
+</script>
+
+<template>
+  <div>
+    <h1>Create Network</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pkg/nowcom/pages/homepage.vue
@@ -0,0 +1,13 @@
+<script>
+export default { name: 'HomePage' };
+</script>
+
+<template>
+  <div>
+    <h1>Nowcom Home Page</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
