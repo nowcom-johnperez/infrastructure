d0b6ab1040f82fde1b24d1ee303a203e6349ce64
Francis Al Victoriano <fvictoriano@now-fvict-mbp.hankeyinvestments.com>
2023-12-04 11:06:03 -0800

    add francis plugin for installation test

---
+++ pkg/francis-test/NewHomeComponent.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name:   'NweHomePage',
+  layout: 'plain'
+};
+</script>
+<template>
+  <div>
+    <h1>Welcome to Rancher</h1>
+    <h3>Welcome 0.1.0</h3>
+  </div>
+</template>
+<style lang="scss" scoped>
+  h1, h3 {
+    margin: 20px 0;
+    text-align: center;
+  }
+
+  h3 {
+    color: #2553FF;
+    font-weight: bold;
+  }
+</style>
