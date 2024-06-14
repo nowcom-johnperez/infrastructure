e7ecfeed11d62de99db81a19554ef428d1ec38fe
John Perez <John.Perez@nowcom.com.ph>
2024-06-15 01:47:45 +0800

    fix timeline design

--- pkg/nowcom/components/TimelineComponent.vue
+++ pkg/nowcom/components/TimelineComponent.vue
@@ -1,16 +1,33 @@
 <template>
-  <div class="timeline-container">
-    <div class="timeline">
-      <div class="event" v-for="event in events" :key="event.date">
-        <div class="event-circle"></div>
-        <div class="event-date">{{ event.date }}</div>
-        <ul class="event-description">
-          <li v-for="(desc, index) in event.descriptions" :key="index">{{ desc }}</li>
-        </ul>
+  <div class="timeline">
+    <div :class="['timeline-top-bar', 'dynamic-width-' + events.length]">
+      <div class="circle circle-big"></div>
+      <div class="vertical-line"></div>
+      <div class="circle circle-big"></div>
+    </div>
+    
+    <div class="timeline-cards-container">
+      <div v-for="(event, index) in events" :key="index" class="timeline-card">
+        <div class="small-circle"></div>
+        <div class="horizontal-line"></div>
+        <div class="box">
+          <div class="top">
+            {{ event.date }}
+          </div>
+          <div class="bottom">
+            <ul class="description-list">
+              <li v-for="(desc, idx) in event.descriptions" :key="idx" class="description-item">
+                <div class="description-icon"></div>
+                <div class="description-text">{{ desc }}</div>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
 
 <script>
 export default {
@@ -72,71 +89,145 @@ export default {
         }
       ],
     };
-  },
+  }
 };
 </script>
 
 <style scoped lang="scss">
-.timeline-container {
-  overflow-x: auto;
-  white-space: nowrap;
-  padding: 20px 0;
-
-  // background: url('../assets/img/bg-timeline.jpg') no-repeat center center;
-  background-size: cover;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-
-  // color: #fff;
-}
-
 .timeline {
   display: flex;
-  align-items: flex-start;
+  flex-direction: column;
+  align-items: center;
+}
+
+.timeline-top-bar {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 0;
+  margin-bottom: -55px;
+}
+
+.circle {
+  background: #0099cc;
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  box-shadow: 0 0 0 2px #fff, 0 0 0 3px #0099cc;
+  margin: 5px;
+  z-index: 1;
+}
+
+.vertical-line {
+  height: 4px;
+  background: #0099cc;
+  display: block;
+  position: relative;
+  flex-grow: 1;
+}
+
+.timeline-cards-container {
+  display: flex;
+  position: relative;
+  overflow-x: auto;
+  width: 100%;
+  padding-bottom: 20px;
+}
+
+.timeline-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 5px;
   position: relative;
 }
 
-.event {
-  display: inline-block;
-  position: relative;
-  text-align: center;
-  padding: 0 40px;
+.small-circle {
+  background: #0099cc;
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  box-shadow: 0 0 0 2px #fff, 0 0 0 3px #0099cc;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.horizontal-line {
+  width: 2px;
+  background: #0099cc;
+  height: 100px;
+  display: block;
+  margin-bottom: 8px;
+  margin-top: 15px;
+}
+
+.box {
+  width: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.box .top {
+  background: #297db4;
+  width: 95%;
+  height: 50px;
+  box-shadow: 0 0 0 2px #fff, 0 0 0 5px #0099cc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  color: #fff;
+  border-radius: 8px 8px 0 0;
+}
+
+.box .bottom {
+  background: #d0d0d0;
+  min-height: 100px;
+  width: 100%;
+  margin-top: 3px;
+  padding: 10px;
+  box-sizing: border-box;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 0 0 8px 8px;
+}
+
+.description-list {
+  list-style: none; /* Remove default list styling */
+  padding: 0;
+  margin: 0;
+}
+
+.description-item {
+  display: flex;
+  align-items: flex-start;
+  margin-bottom: 8px;
+}
+
+.description-icon {
+  width: 16px;
+  height: 16px;
+  background: #297db4;
+  border-radius: 50%;
+  margin-right: 8px;
+  margin-top: 3px;
   flex-shrink: 0;
 }
 
-.event:not(:last-child)::after {
-  content: '';
-  position: absolute;
-  top: 10px;
-  left: 100%;
-  width: 60px;
-  height: 2px;
-  background-color: #ccc;
+.description-text {
+  color: #333;
+  font-size: 14px;
+  line-height: 1.4;
 }
 
-.event-circle {
-  width: 20px;
-  height: 20px;
-  background-color: #000000;
-  border-radius: 50%;
-  margin: 0 auto 10px;
-}
-
-.event-date {
-  font-weight: bold;
-  margin-bottom: 10px;
-}
-
-.event-description {
-  max-width: 200px;
-  white-space: normal;
-  text-align: left;
-  padding-left: 20px;
-  list-style-type: disc;
-}
-
-.event-description li {
-  margin-bottom: 5px;
-}
+.dynamic-width-1 .vertical-line { width: 100px; }
+.dynamic-width-2 .vertical-line { width: 200px; }
+.dynamic-width-3 .vertical-line { width: 300px; }
+.dynamic-width-4 .vertical-line { width: 400px; }
+.dynamic-width-5 .vertical-line { width: 500px; }
+.dynamic-width-6 .vertical-line { width: 600px; }
+.dynamic-width-7 .vertical-line { width: 700px; }
+.dynamic-width-8 .vertical-line { width: 800px; }
 </style>
