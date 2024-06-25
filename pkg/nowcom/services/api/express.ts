9709b9f784c42eb56eb3031c9d950e5dcf51a10e
John Perez <John.Perez@nowcom.com.ph>
2024-06-26 01:03:42 +0800

    fixes api url

--- pkg/nowcom/services/api/express.ts
+++ pkg/nowcom/services/api/express.ts
@@ -2,17 +2,13 @@ import { INSTANCE_V2 } from "../../config/axios";
 
 export const expressService = {
     getAllNetworks: async () => {
-        const res: any = await INSTANCE_V2.get(`/apis/packetlifter.dev/v1/namespaces/default/subnets`)
+        const res: any = await INSTANCE_V2.get(`/apis/packetlifter.io/v1/namespaces/default/subnets`)
         return res.data.items
-    },
-
-    getNetworkTranslations: async () => {
-        return await INSTANCE_V2.get(`/apis/packetlifter.dev/v1/addresstranslations`)
     },
     
     patchExpressSubnet: async (networkName: string, expressData: any) => {
         return await INSTANCE_V2.patch(
-            `apis/packetlifter.dev/v1/namespaces/default/subnets/${networkName}`,
+            `apis/packetlifter.io/v1/namespaces/default/subnets/${networkName}`,
             expressData,
             { headers: { 'Content-Type': 'application/merge-patch+json' } }
         )
--- pkg/nowcom/services/api/vnet.ts
+++ pkg/nowcom/services/api/vnet.ts
@@ -2,30 +2,30 @@ import { INSTANCE_V2 } from "../../config/axios";
 
 export const vNetService = {
     getNetworks: async () => {
-        return await INSTANCE_V2.get(`/apis/packetlifter.dev/v1/vnets`)
+        return await INSTANCE_V2.get(`/apis/packetlifter.io/v1/vnets`)
     },
 
     getNetworkTranslations: async () => {
-        return await INSTANCE_V2.get(`/apis/packetlifter.dev/v1/addresstranslations`)
+        return await INSTANCE_V2.get(`/apis/packetlifter.io/v1/addresstranslations`)
     },
 
     createNetwork: async (data: any) => {
-        return await INSTANCE_V2.post(`/apis/packetlifter.dev/v1/namespaces/default/vnets`, data);
+        return await INSTANCE_V2.post(`/apis/packetlifter.io/v1/namespaces/default/vnets`, data);
     },
 
     deleteNetwork: async (networkName: string) => {
-        return await INSTANCE_V2.delete(`/apis/packetlifter.dev/v1/namespaces/default/vnets/${networkName}`)
+        return await INSTANCE_V2.delete(`/apis/packetlifter.io/v1/namespaces/default/vnets/${networkName}`)
     },
 
     getSubnetByName: async (networkName: string) => {
         return await INSTANCE_V2.get(
-            `apis/packetlifter.dev/v1/namespaces/default/vnets/${networkName}`
+            `apis/packetlifter.io/v1/namespaces/default/vnets/${networkName}`
         )
     },
     
     patchSubnet: async (networkName: string, vNetData: any) => {
         return await INSTANCE_V2.patch(
-            `apis/packetlifter.dev/v1/namespaces/default/vnets/${networkName}`,
+            `apis/packetlifter.io/v1/namespaces/default/vnets/${networkName}`,
             vNetData,
             { headers: { 'Content-Type': 'application/merge-patch+json' } }
         )
