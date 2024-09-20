6dae8f619cc8d17bb5db23dea24ebb7c6c80c71b
John Perez <John.Perez@nowcom.com.ph>
2024-09-21 02:48:13 +0800

    update v0.3.2

--- pkg/nowcom/components/environment/EnvironmentView.vue
+++ pkg/nowcom/components/environment/EnvironmentView.vue
@@ -1,17 +1,49 @@
 <template>
-  <div>
-    <h2 class="text-label">{{ currentObj.name }}</h2>
+  <div class="environment-detail-view">
+    <div class="environment-detail-header">
+      <div class="environment-detail-info">
+        <h2 class="section-title">Environment Information</h2>
+        <p class="section-subtitle">Details about the environment.</p>
+      </div>
+    </div>
+    
+
     <Tabs :list="['Basic', 'Additional Information']" :current="currentTabIndex" @set-active="setTab"/>
 
     <div class="tab-content-container mt-40">
+      <!-- Basic Information Tab -->
       <div class="tab-content" :class="{ 'show': currentTabIndex === 0 }">
-        Basic Info Here
+        <div class="details-table">
+          <div class="row-detail">
+            <div class="row-label">Name</div>
+            <div class="row-val">{{ currentObj.name }}</div>
+          </div>
+          
+          <div class="row-detail">
+            <div class="row-label">Size</div>
+            <div class="row-val">{{ currentObj.size }}</div>
+          </div>
+
+          <div class="row-detail">
+            <div class="row-label">Status</div>
+            <div class="row-val">
+              <BadgeState
+                :label="currentObj.status"
+                :color="badgeColor"
+              />
+            </div>
+          </div>
+
+          <div class="row-detail">
+            <div class="row-label">Description</div>
+            <div class="row-val">{{ currentObj.description }}</div>
+          </div>
+        </div>
       </div>
 
+      <!-- Additional Information Tab -->
       <div class="tab-content" :class="{ 'show': currentTabIndex === 1 }">
-        <pre>
-          {{ currentObj }}
-        </pre>
+        <p>Additional info will be set here</p>
       </div>
     </div>
   </div>
@@ -19,6 +51,8 @@
 
 <script>
 import Tabs from '../common/Tabs'
+import { BadgeState } from '@components/BadgeState';
+import { getBadgeColor } from '../../services/helpers/environment';
 export default {
   name: 'EnvironmentView',
   props: {
@@ -28,7 +62,8 @@ export default {
     }
   },
   components: {
-    Tabs
+    Tabs,
+    BadgeState
   },
   data() {
     return {
@@ -39,6 +74,77 @@ export default {
     setTab (tabIndex) {
       this.currentTabIndex = tabIndex
     },
+  },
+  computed: {
+    statusClass() {
+      return this.currentObj.status === 'Approved' ? 'approved' : 'pending';
+    },
+    badgeColor () {
+      return getBadgeColor(this.currentObj.status)
+    },
+    serviceIcon () {
+      return getServiceIcon(this.currentObj.service)
+    }
   }
 }
 </script>
+
+<style scoped>
+.environment-detail-view {
+  border-radius: 8px;
+}
+
+.environment-detail-header {
+  display: flex;
+}
+
+.section-title {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.section-subtitle {
+  font-size: 14px;
+  color: #6B7280; /* Light gray */
+  margin-bottom: 30px;
+}
+
+.details-table {
+  width: 100%;
+  border-spacing: 0 10px;
+}
+
+.row-detail {
+  display: grid;
+  grid-template-columns: 130px 1fr;
+  gap: 20px;
+  padding: 10px 0;
+  border-bottom: 0.5px solid #6B7280;
+}
+
+.row-label {
+  font-weight: 600;
+  font-size: 16px;
+}
+
+.row-val {
+  font-size: 16px;
+}
+
+.service-status {
+  padding: 5px 10px;
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: bold;
+}
+
+.additional-info {
+  background-color: #f9fafb;
+  padding: 20px;
+  border-radius: 8px;
+  margin-top: 20px;
+  font-family: monospace;
+  color: #374151;
+}
+</style>
--- pkg/nowcom/components/shared-services/SharedServiceView.vue
+++ pkg/nowcom/components/shared-services/SharedServiceView.vue
@@ -1,15 +1,57 @@
 <template>
-  <div>
-    <h2 class="text-label">{{ currentObj.name }}</h2>
+  <div class="service-detail-view">
+    <div class="service-detail-header">
+      <div class="service-detail-img mr-20">
+        <i :class="`fa fa-4x ${serviceIcon}`"></i>
+      </div>
+      <div class="service-detail-info">
+        <h2 class="section-title">Shared Service Information</h2>
+        <p class="section-subtitle">Details about the service and its environment.</p>
+      </div>
+    </div>
+    
+
     <Tabs :list="['Basic', 'Additional Information']" :current="currentTabIndex" @set-active="setTab"/>
 
     <div class="tab-content-container mt-40">
+      <!-- Basic Information Tab -->
       <div class="tab-content" :class="{ 'show': currentTabIndex === 0 }">
-        Basic Info Here
+        <div class="details-table">
+          <div class="row-detail">
+            <div class="row-label">Service Name</div>
+            <div class="row-val">{{ currentObj.name }}</div>
+          </div>
+          
+          <div class="row-detail">
+            <div class="row-label">Service Type</div>
+            <div class="row-val">{{ currentObj.service }}</div>
+          </div>
+
+          <div class="row-detail">
+            <div class="row-label">Environment</div>
+            <div class="row-val">{{ currentObj.environment }}</div>
+          </div>
+
+          <div class="row-detail">
+            <div class="row-label">Status</div>
+            <div class="row-val">
+              <BadgeState
+                :label="currentObj.status"
+                :color="badgeColor"
+              />
+            </div>
+          </div>
+
+          <div class="row-detail">
+            <div class="row-label">Description</div>
+            <div class="row-val">{{ currentObj.description }}</div>
+          </div>
+        </div>
       </div>
 
+      <!-- Additional Information Tab -->
       <div class="tab-content" :class="{ 'show': currentTabIndex === 1 }">
-        <pre>
+        <pre class="additional-info">
           {{ currentObj }}
         </pre>
       </div>
@@ -19,6 +61,8 @@
 
 <script>
 import Tabs from '../common/Tabs'
+import { BadgeState } from '@components/BadgeState';
+import { getBadgeColor, getServiceIcon } from '../../services/helpers/shared-service';
 export default {
   name: 'SharedServiceView',
   props: {
@@ -28,7 +72,8 @@ export default {
     }
   },
   components: {
-    Tabs
+    Tabs,
+    BadgeState
   },
   data() {
     return {
@@ -39,6 +84,76 @@ export default {
     setTab (tabIndex) {
       this.currentTabIndex = tabIndex
     },
+  },
+  computed: {
+    statusClass() {
+      return this.currentObj.status === 'Approved' ? 'approved' : 'pending';
+    },
+    badgeColor () {
+      return getBadgeColor(this.currentObj.status)
+    },
+    serviceIcon () {
+      return getServiceIcon(this.currentObj.service)
+    }
   }
 }
 </script>
+
+<style scoped>
+.service-detail-view {
+  border-radius: 8px;
+}
+
+.service-detail-header {
+  display: flex;
+}
+
+.section-title {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.section-subtitle {
+  font-size: 14px;
+  color: #6B7280; /* Light gray */
+  margin-bottom: 30px;
+}
+
+.details-table {
+  width: 100%;
+  border-spacing: 0 10px;
+}
+
+.row-detail {
+  display: grid;
+  grid-template-columns: 130px 1fr;
+  gap: 20px;
+  padding: 10px 0;
+}
+
+.row-label {
+  font-weight: 600;
+  font-size: 16px;
+}
+
+.row-val {
+  font-size: 16px;
+}
+
+.service-status {
+  padding: 5px 10px;
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: bold;
+}
+
+.additional-info {
+  background-color: #f9fafb;
+  padding: 20px;
+  border-radius: 8px;
+  margin-top: 20px;
+  font-family: monospace;
+  color: #374151;
+}
+</style>
