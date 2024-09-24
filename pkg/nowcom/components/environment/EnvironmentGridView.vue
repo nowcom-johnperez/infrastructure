7c1008815646721e6efe6594de509d9197f8d512
John Perez <John.Perez@nowcom.com.ph>
2024-09-25 02:22:49 +0800

    pagination

--- pkg/nowcom/components/environment/EnvironmentGridView.vue
+++ pkg/nowcom/components/environment/EnvironmentGridView.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="environment-grid">
-    <EnvironmentCard v-for="service in list" :key="service.name" :service="service" @view-click="viewItem" />
+  <div>
+    <div class="environment-grid">
+      <EnvironmentCard v-for="service in list" :key="service.name" :service="service" @view-click="viewItem" />
+    </div>
+
+    <div class="pagination-controls">
+      <button :disabled="currentPage === 1" @click="prevPage" class="btn role-primary btn-sm">Previous</button>
+      <span>Page {{ currentPage }} of {{ totalPages }}</span>
+      <button :disabled="currentPage === totalPages" @click="nextPage" class="btn role-primary btn-sm">Next</button>
+    </div>
   </div>
 </template>
 
@@ -20,7 +28,20 @@ export default {
     EnvironmentCard
   },
   data() {
-    return {}
+    return {
+      currentPage: 1,
+      pageSize: 6
+    }
+  },
+  computed: {
+    paginatedList() {
+      const start = (this.currentPage - 1) * this.pageSize;
+      const end = this.currentPage * this.pageSize;
+      return this.list.slice(start, end);
+    },
+    totalPages() {
+      return Math.ceil(this.list.length / this.pageSize);
+    }
   },
   methods: {
     viewItem(item) {
@@ -28,6 +49,16 @@ export default {
         item,
         component: EnvironmentView
       })
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
     }
   }
 }
@@ -38,5 +69,16 @@ export default {
     display: grid;
     grid-gap: 1rem;
     grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  }
+
+  .pagination-controls {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 1rem;
+
+    button {
+      margin: 0 1rem;
+    }
   }
 </style>
--- pkg/nowcom/components/shared-services/SharedServicesGrid.vue
+++ pkg/nowcom/components/shared-services/SharedServicesGrid.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="shared-services-grid">
-    <ServiceCard v-for="service in list" :key="service.name" :service="service" @view-click="viewItem" />
+  <div>
+    <div class="shared-services-grid">
+      <ServiceCard
+        v-for="service in paginatedList"
+        :key="service.name"
+        :service="service"
+        @view-click="viewItem"
+      />
+    </div>
+
+    <!-- Pagination Controls -->
+    <div class="pagination-controls">
+      <button :disabled="currentPage === 1" @click="prevPage" class="btn role-primary btn-sm">Previous</button>
+      <span>Page {{ currentPage }} of {{ totalPages }}</span>
+      <button :disabled="currentPage === totalPages" @click="nextPage" class="btn role-primary btn-sm">Next</button>
+    </div>
   </div>
 </template>
 
@@ -8,6 +22,7 @@
 import ServiceCard from './ServiceCard.vue';
 import SharedServiceView from './SharedServiceView.vue';
 import { EventBus } from '../../config/event-bus';
+
 export default {
   name: 'SharedServicesGridView',
   props: {
@@ -20,23 +35,57 @@ export default {
     ServiceCard
   },
   data() {
-    return {}
+    return {
+      currentPage: 1,
+      pageSize: 6
+    };
+  },
+  computed: {
+    paginatedList() {
+      const start = (this.currentPage - 1) * this.pageSize;
+      const end = this.currentPage * this.pageSize;
+      return this.list.slice(start, end);
+    },
+    totalPages() {
+      return Math.ceil(this.list.length / this.pageSize);
+    }
   },
   methods: {
-    viewItem (item) {
+    viewItem(item) {
       EventBus.$emit('component-view', {
         item,
         component: SharedServiceView
-      })
+      });
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-  .shared-services-grid {
-    display: grid;
-    grid-gap: 1rem;
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+.shared-services-grid {
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+}
+
+.pagination-controls {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 1rem;
+
+  button {
+    margin: 0 1rem;
   }
+}
 </style>
