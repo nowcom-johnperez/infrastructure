30888ab3905ff2eb27894a018c46ba5e0de59f10
John Perez <John.Perez@nowcom.com.ph>
2024-11-01 03:28:00 +0800

    use rancher url

--- pkg/nowcom/services/api/environment.ts
+++ pkg/nowcom/services/api/environment.ts
@@ -1,9 +1,10 @@
-import { INSTANCE_V4 } from "../../config/axios";
+import { INSTANCE_V4, INSTANCE_V3 } from "../../config/axios";
 import { getConfig } from "../../config/api";
 const { TRIDENT_API, API_VERSION } = getConfig();
 export const environmentService = {
     getAll: async () => {
-        const res: any =  await INSTANCE_V4.get(`/apis/trident.dev/${API_VERSION}/namespaces/default/createenvs`)
+        // const res: any =  await INSTANCE_V4.get(`/apis/trident.dev/${API_VERSION}/namespaces/default/createenvs`)
+        const res: any =  await INSTANCE_V3.get(`/k8s/clusters/c-m-7mbwkw9d/apis/trident.dev/${API_VERSION}/namespaces/default/createenvs`)
         return res.data.items
     }
 }
